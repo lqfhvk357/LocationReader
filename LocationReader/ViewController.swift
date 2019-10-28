30f7f29ffdc57486700885d49c20c7df3b8cb394
lc <58054863@qq.com>
2019-10-28 12:02:30 +0800

    init

---
+++ LocationReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationReader
+//
+//  Created by 罗超 on 2019/10/28.
+//  Copyright © 2019 罗超. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
